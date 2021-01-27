8d02bd73b56617e713c8cc01d438dbe3d6a90bee
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-01-27 17:00:24 +0200

    add login

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,51 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+
+const mongoose = require("mongoose");
+
+const db = "mongodb://localhost:27017/angularAuth";
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log("connected to mongodb");
 
 router.get("/", (req, res) => {
   res.send("From API route");
+});
+
+router.post("/register", (req, res) => {
+  let userData = req.body;
+  let user = new User(userData);
+  user.save((error, registeredUser) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(200).send(registeredUser);
+    }
+  });
+});
+
+router.post("/login", (req, res) => {
+  // get user data from request body
+  const userData = req.body;
+  // check if email exist in database
+  User.findOne({ email: userData.email }, (error, user) => {
+    // if error in db
+    if (error) {
+      console.log(error);
+    } else {
+
+      if (!user) {
+        res.status(401).send("Invalid email");
+      } else {
+        if (user.password !== userData.password) {
+          res.status(401).send("Invalid Password");
+        } else {
+          res.status(200).send(user);
+        }
+      }
+    }
+  });
 });
 
 module.exports = router;
